b97cca11fecf6ca61a94c8410696de4cecb19b24
vamsi0004 <85745642+vamsi0004@users.noreply.github.com>
2022-04-02 21:46:39 +0530

    Add files via upload

---
+++ Clang/code2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int a=10,b=20,area;
+    area = a*b;
+    printf("Area of Rectangle is %d",area);
+
+}
---
+++ Clang/code5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,area,perimeter;
+    a=5,b=6;
+    area=a*b;
+    perimeter = 2*(a+b);
+    printf("Area is %d and Perimeter is %d",area,perimeter);
+}
