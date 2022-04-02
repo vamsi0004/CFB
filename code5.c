4a1e6c41ba2509b8231e7e7c3493f31bb9aaabe3
vamsi0004 <85745642+vamsi0004@users.noreply.github.com>
2022-04-02 21:47:36 +0530

    Delete code5.c

--- code5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int a,b,area,perimeter;
-    a=5,b=6;
-    area=a*b;
-    perimeter = 2*(a+b);
-    printf("Area is %d and Perimeter is %d",area,perimeter);
-}
