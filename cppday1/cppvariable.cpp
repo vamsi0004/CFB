1f99a0282b494d77012a48b5edf47d6fe16ddfe9
vamsi0004 <85745642+vamsi0004@users.noreply.github.com>
2022-04-15 20:27:37 +0530

    Add files via upload

---
+++ cppday1/cppvariable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string charname = "mahesh";
+    int charage;
+    charage=20;
+    cout <<"There once was a man named " << charname <<endl;
+    cout <<"He was " <<charage <<" years old "<< endl;
+    charname="balaji";
+    cout <<"HE liked the name " <<charname<<endl;
+    cout << "but did not like "<<charage<<endl;
+    return 0;
+}
